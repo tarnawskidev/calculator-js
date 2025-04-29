da206b6e1b158fedfec2b2092ac1d9d95c11a801
Clara <dtarnawskilts@gmail.com>
2025-04-29 10:36:02 -0500

    Added JS code to generate the calculator keypad

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-
+const calcBody = document.querySelector("#calcBody");
 
 let number1 = "";
 let number2 = "";
 let operator = "";
 
 
+generateCalc();
+
+//function that performs operation on two numbers
 function operate(num1, num2, operator) {
     switch(operator){
         case "add":
-            add(num1, num2)
-            break;
+            return add(num1, num2);
         case "subtract":
-            subtract(num1, num2)
-            break;
+            return subtract(num1, num2);
         case "multiply":
-            multiply(num1, num2)
-            break;
+            return multiply(num1, num2);
         case "divide":
-            divide(num1, num2)
-            break;
+            return divide(num1, num2);
     }
 }
 
+//functions for math logic
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -36,4 +36,18 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     return num1 / num2;
+}
+
+
+function generateCalc() {
+    for (let i=9; i >-1; i--) {
+        let button = document.createElement("div");
+        button.id = i;
+        button.textContent = i;
+        button.className ="calcButton";
+        button.addEventListener("click", () => {
+            console.log(button.id)
+        })
+        calcBody.appendChild(button);
+    }
 }
