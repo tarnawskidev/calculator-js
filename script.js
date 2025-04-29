18d05a2253bf6a48427f3eee7b1ef74a5fda89a9
Clara <dtarnawskilts@gmail.com>
2025-04-29 10:07:30 -0500

    Set up project file as well as basic JS logic

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+let number1 = "";
+let number2 = "";
+let operator = "";
+
+
+function operate(num1, num2, operator) {
+    switch(operator){
+        case "add":
+            add(num1, num2)
+            break;
+        case "subtract":
+            subtract(num1, num2)
+            break;
+        case "multiply":
+            multiply(num1, num2)
+            break;
+        case "divide":
+            divide(num1, num2)
+            break;
+    }
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
